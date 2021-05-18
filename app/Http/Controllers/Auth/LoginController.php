3494d9509654b90ccdcf58f86226d04bf14a9ee0
hafiyyanabdulaziz <hafiyyan.123@gmail.com>
2021-05-18 07:46:42 +0700

    edit login blade dan login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -38,11 +39,16 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    public function credentials(Request $request)
+    protected function credentials(Request $request)
     {
         return [
-            'mail' => $request->get('username'),
+            'uid' => $request->get('username'),
             'password' => $request->get('password'),
         ];
+    }
+
+    public function username()
+    {
+        return 'username';
     }
 }
